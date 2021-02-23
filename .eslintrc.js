9ca396eabf3598c98ec0f9e4d71863bd5a34b697
Vishal Yadav <68839171+VishalYadav0757@users.noreply.github.com>
2021-02-23 10:34:15 +0530

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'wesbos',
+  rules: {
+    'react/prop-types': 0,
+    'react/display-name': 0
+  }
+};
---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import FileDecoder from './src/FileDecoder';
+
+const App = () => <FileDecoder />;
+
+export default App;
